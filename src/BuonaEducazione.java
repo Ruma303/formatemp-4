41629f68588a9bf6bd9488372f47435f90804b7c
Ruma <matteoh8@hotmail.it>
2025-02-24 11:40:21 +0100

    .

--- src/BuonaEducazione.java
+++ src/BuonaEducazione.java
@@ -1,16 +1,35 @@
 
 public class BuonaEducazione {
+	
+	// Variabili di istanza
+	// Per usarli bisogna istanziare un oggetto di questa classe
+	// In questo esempio, assegneremo i valori di
+	// queste variabili dopo aver istanziato la classe nel Main
+	public Double a;
+	public Double b;
+	
+	// Variabile di classe
+	// Si possono utilizzare senza istanziare un oggetto
+	public static String c = "Valore condiviso in ogni istanza";
 
 	// Metodi "di istanza".
 	// Richiedono l'istanziamento di questa classe
 	// per poter utilizzare i metodi
 	public void saluta(){
-		System.out.println("Buona educazione!");	
+		System.out.println("\tBuona educazione!");	
 	}
 	
 	public void ringrazia(){
-		System.out.println("Grazie!");	
+		System.out.println("\tGrazie!");	
 	}
+	
+	public Double sommaNonStatica(Double a, Double b) {
+		// Il this.a viene utilizzato per accedere alle
+		//  variabili di istanza: a, b
+		return this.a + this.b;
+	}
+	
+	
 	
 	// Metodi statici "di classe"
 	// Con la parola chiave static, 
@@ -20,6 +39,11 @@ public class BuonaEducazione {
 		return "Marco";
 	}
 	
+	// I parametri di questi metodi statici arrivano
+	// dai valori passati alla chiamata del metodo
+	// nella classe Main, utilizzando Scanner
+	// Non sono le variabili di istanza di questa classe 
+	// definiti all'inizio.
 	public static Double somma(Double a, Double b) {
 		return a + b;
 	}
--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,39 @@ public class Main {
 		
 		// Creazione di un oggetto della classe BuonaEducazione
 		BuonaEducazione be = new BuonaEducazione();	
+		BuonaEducazione be2 = new BuonaEducazione();			
 		
 		// Metodi di istanza: richiedono l'istanza di un oggetto
+		System.out.println("Metodi di istanza:");
+		System.out.println("\tIl nome del programmatore è " + be.getNomeProgrammatore());
+		
 		be.saluta();
 		be.ringrazia();
+
+		System.out.println("\r\n================================\n");
 		
-		System.out.println("Metodi di istanza");
-		System.out.println("Il nome del programmatore è " + be.getNomeProgrammatore());
+		// Assegnamo le variabili di istanza all'oggetto be 
+		be.a = 10.0;
+		be.b = 20.0;
+		
+		System.out.println("Valori assegnati alle variabili dell'oggetto be:");
+		System.out.println("\ta = " + be.a + ", b = " + be.b);
+		
+		// L'oggetto be2 non ha le stesse variabili di istanza
+		// Si chiamano variabili di istanza in quanto il loro valore
+		// è specifico per ogni oggetto, lo stato non è condiviso
+		// tra oggetti.
+		
+		System.out.println("Valori assegnati alle variabili dell'oggetto be2:");
+		System.out.println("\ta = " + be2.a + ", b = " + be2.b);
+		
+		// Viceversa, le variabili di classe (con la parola static)
+		// sono condivise e accessibili da tutti gli oggetti della classe
+        System.out.println("\nVariabile di classe:");
+        System.out.println("\tOggetto be, valore c = " + be.c);
+        System.out.println("\tOggetto be2, valore c = " + be.c);
         
+        System.out.println("\r\n================================");
 
 		// In questo esempio prendiamo i numeri da tastiera
 		Scanner scanner = new Scanner(System.in);
@@ -23,7 +48,12 @@ public class Main {
         
         System.out.println("\nInserisci un secondo numero, anche decimale:");
         Double n2 = scanner.nextDouble();
-		
+        
+        scanner.close();
+        
+        
+        
+        
         // Metodi statici: non richiedono l'istanza di un oggetto
 		
         Double sum = BuonaEducazione.somma(n1, n2);
@@ -32,12 +62,12 @@ public class Main {
         Double div = BuonaEducazione.divisione(n1, n2);
         Double mod = BuonaEducazione.divisioneIntera(n1, n2);
         
-        System.out.println("\nMetodi di classe");
-        System.out.println("La somma di " + n1 + " e " + n2 + " è " + sum);    
-        System.out.println("La differenza di " + n1 + " e " + n2 + " è " + diff);    
-        System.out.println("Il prodotto di " + n1 + " e " + n2 + " è " + mul);
-        System.out.println("La divisione di " + n1 + " e " + n2 + " è " + div);
-        System.out.println("Il resto della divisione intera di " + n1 + " e " + n2 + " è " + mod);
+        System.out.println("\nMetodi di classe:");
+        System.out.println("\tLa somma di " + n1 + " e " + n2 + " è " + sum);    
+        System.out.println("\tLa differenza di " + n1 + " e " + n2 + " è " + diff);    
+        System.out.println("\tIl prodotto di " + n1 + " e " + n2 + " è " + mul);
+        System.out.println("\tLa divisione di " + n1 + " e " + n2 + " è " + div);
+        System.out.println("\tIl resto della divisione intera di " + n1 + " e " + n2 + " è " + mod);
 	}
 
 }
