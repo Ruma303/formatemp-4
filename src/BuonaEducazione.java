c275a5a4bf42cb97e347f6ec096dcbf912a2f969
Ruma <matteoh8@hotmail.it>
2025-02-24 11:05:29 +0100

    metodi statici e non

--- src/BuonaEducazione.java
+++ src/BuonaEducazione.java
@@ -1,16 +1,43 @@
 
 public class BuonaEducazione {
 
-	public static void saluta(){
+	// Metodi "di istanza".
+	// Richiedono l'istanziamento di questa classe
+	// per poter utilizzare i metodi
+	public void saluta(){
 		System.out.println("Buona educazione!");	
 	}
 	
-	public static void ringrazia(){
+	public void ringrazia(){
 		System.out.println("Grazie!");	
 	}
 	
+	// Metodi statici "di classe"
+	// Con la parola chiave static, 
+	// possono essere utilizzati senza istanziare la classe
+	// ma possono essere invocati direttamente dalla classe
 	public static String getNomeProgrammatore() {
 		return "Marco";
+	}
+	
+	public static Double somma(Double a, Double b) {
+		return a + b;
+	}
+	
+	public static Double differenza(Double a, Double b) {	
+		return a - b;
+	}
+	
+	public static Double moltiplicazione(Double a, Double b) {	
+		return a * b;
+	}
+	
+	public static Double divisione(Double a, Double b) {	
+		return a / b;
+	}
+	
+	public static Double divisioneIntera(Double a, Double b) {	
+		return a % b;
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,30 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		// Creazione di un oggetto della classe BuonaEducazione
 		BuonaEducazione be = new BuonaEducazione();	
 		
+		// Metodi di istanza: richiedono l'istanza di un oggetto
 		be.saluta();
 		be.ringrazia();
 		
-        System.out.println("Il nome del programmatore è " + be.getNomeProgrammatore());
+		System.out.println("Metodi di istanza");
+		System.out.println("Il nome del programmatore è " + be.getNomeProgrammatore());
+        
+        
+        // Metodi statici: non richiedono l'istanza di un oggetto
+        Double sum = BuonaEducazione.somma(5.0, 3.0);
+        Double diff = BuonaEducazione.differenza(15.0, -3.0);
+        Double mul = BuonaEducazione.moltiplicazione(2.0, 3.0);
+        Double div = BuonaEducazione.divisione(15.0, 3.0);
+        Double mod = BuonaEducazione.divisioneIntera(10.0, 4.0);
+        
+        System.out.println("\n Metodi di classe");
+        System.out.println("La somma di 5.0 e 3.0 è " + sum);	
+        System.out.println("La differenza tra 15.0 e -3.0 è " + diff);	
+        System.out.println("Il prodotto di 2.0 e 3.0 è " + mul);
+        System.out.println("La divisione tra 15.0 e 3.0 è " + div);
+        System.out.println("Il resto della divisione intera tra 10.0 e 4.0 è " + mod);
 	}
 
 }
