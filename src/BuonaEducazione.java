c442363a4b559bdc7bd4105f6f3956af61d26ef3
Ruma <matteoh8@hotmail.it>
2025-02-24 10:33:29 +0100

    init

---
+++ src/BuonaEducazione.java
@@ -0,0 +1,16 @@
+
+public class BuonaEducazione {
+
+	public static void saluta(){
+		System.out.println("Buona educazione!");	
+	}
+	
+	public static void ringrazia(){
+		System.out.println("Grazie!");	
+	}
+	
+	public static String getNomeProgrammatore() {
+		return "Marco";
+	}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		BuonaEducazione be = new BuonaEducazione();	
+		
+		be.saluta();
+		be.ringrazia();
+		
+        System.out.println("Il nome del programmatore è " + be.getNomeProgrammatore());
+	}
+
+}
